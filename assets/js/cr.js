2cf6368c57675d311600eed58f918cf6322f6b6f
Taner1998 <taberandwords@hotmail.com>
2021-06-08 12:48:24 +0400

    licence infos included

---
+++ assets/js/cr.js
@@ -0,0 +1,3 @@
+console.log('All Rights Reserved by Taneristique ©')
+console.log("Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License included.")
+console.log("Forbidden to commercial use!")

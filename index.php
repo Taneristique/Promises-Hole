61b0f9c1a21eecccde173b7cc87c0834e3d6c883
Taner1998 <taberandwords@hotmail.com>
2021-06-08 12:58:20 +0400

    index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    function promiseBuilder(){
+    echo '<html>
+          <head>
+          <link rel="stylesheet" href="assets/css/style.css">
+          <title>Promises</title>
+          </head>';
+    echo '<body><h1>';
+    echo 'Here Are The Promises';
+    echo"</h1>
+         <div class='first'>{open}</div>
+         <div class='second'>{close}</div>
+         <div class='third'>{open}</div>
+         <div class='forth'>{close}</div>
+         <script src='assets/js/promise.js'></script>
+         <script src='assets/js/cr.js'></script>
+         </body></html>";}
+    promiseBuilder();
+?>
